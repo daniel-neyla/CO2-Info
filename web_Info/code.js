c20c1afa22d08628cf7f616bea9e0061c0cf08c2
thomas <thornung@gmail.com>
2019-09-29 10:21:45 +0200

    Datein Upload

--- web_Info/code.js
+++ web_Info/code.js
@@ -6,17 +6,19 @@ var Fahrad_C = 0;
 var Bahn_C = 60;
 var Flugzeug_C = 200;
 
-var Austoß_Woche = (11 * 1000000) / 365;//3.650
-var Austoß_Woche_Sol = (2.7 * 1000000) / 365;
+var Colebstof = [Auto_C,Bahn_C,Flugzeug_C,Fahrad_C];
 
 
-var Hanauer_Landstr_bis_Riedberg =   13;
-var Frankfurt_bis_Berlin =          560;
-var Frankfurt_bis_Lisabon =         1983;
-var Frankfurt_bis_Kapstadt =        11282;
+var Austoß_Woche = (11 * 1000000) / 52;//3.650
+var Austoß_Woche_Sol = (2.7 * 1000000) / 52;
+
+
+var Hanauer_Landstr_bis_Riedberg =   [13,13,13];
+var Frankfurt_bis_Berlin =          [560,560,560];
+var Frankfurt_bis_Lisabon =         [1983,1983,1983];
+var Frankfurt_bis_Kapstadt =        [11282,11282,11282];
 
 function onBottumClickA() {
-    //console.log("onBottumClick: " + Austoß_Woche);
     var Strecke = Hanauer_Landstr_bis_Riedberg;
     document.getElementById("Auswahl").innerText = "Hanauer Landstr. bis Riedberg";
     document.getElementById("km").innerText = Strecke;
@@ -24,23 +26,35 @@ function onBottumClickA() {
     var a_Auto = document.getElementById("austoß_Auto");
     var a_Flugzeug = document.getElementById("austoß_Flugzeug");
     var a_Bahn = document.getElementById("austoß_Bahn");
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
-
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+   
     var p_Auto = document.getElementById("proßent_Auto");
-    var p_Fahrad = document.getElementById("proßent_Fahrad");
     var p_Bahn = document.getElementById("proßent_Bahn");
     var p_Flugzeug = document.getElementById("proßent_Flugzeug");
     
-    a_Auto.innerText    = (Strecke * Auto_C);
-    a_Bahn.innerText    = (Strecke * Bahn_C);
-    a_Flugzeug.innerText= (Strecke * Flugzeug_C);
-    a_Fahrad.innerText  = (Strecke * Fahrad_C);
+    var po_Auto = document.getElementById("proßent_Soll_Auto");
+    var po_Bahn = document.getElementById("proßent_Soll_Bahn");
+    var po_Flugzeug = document.getElementById("proßent_Soll_Flugzeug");
 
-    p_Auto.innerText    = (Strecke * Auto_C)    / Austoß_Woche;
-    p_Bahn.innerText    = (Strecke * Bahn_C)    / Austoß_Woche;
-    p_Flugzeug.innerText= (Strecke * Flugzeug_C)/ Austoß_Woche;
-    p_Fahrad.innerText  = (Strecke * Fahrad_C)  / Austoß_Woche;
+    a_Auto.innerText    = (Strecke[0] * Auto_C);
+    a_Bahn.innerText    = (Strecke[1] * Bahn_C);
+    a_Flugzeug.innerText= "/";
+   
+    p_Auto.innerText    = round(((Strecke[0] * Auto_C)    / Austoß_Woche) * 100);
+    p_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche) * 100);
+    p_Flugzeug.innerText= "/";
+    
+
+    po_Auto.innerText   = round(((Strecke[0] * Auto_C)    / Austoß_Woche_Sol) * 100);
+    po_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche_Sol) * 100);
+    po_Flugzeug.innerText= "/";
+
+    
+    
+
+   
+    document.getElementById("austoß_Fahrad").innerText  = "0";
+    document.getElementById("proßent_Fahrad").innerText  = "0";
+    document.getElementById("proßent_Soll_Fahrad").innerText  = "0";
 }
 
 function onBottumClickB() {
@@ -52,23 +66,34 @@ function onBottumClickB() {
     var a_Auto = document.getElementById("austoß_Auto");
     var a_Flugzeug = document.getElementById("austoß_Flugzeug");
     var a_Bahn = document.getElementById("austoß_Bahn");
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+    
 
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
     var p_Auto = document.getElementById("proßent_Auto");
-    var p_Fahrad = document.getElementById("proßent_Fahrad");
     var p_Bahn = document.getElementById("proßent_Bahn");
     var p_Flugzeug = document.getElementById("proßent_Flugzeug");
     
-    a_Auto.innerText    = (Strecke * Auto_C);
-    a_Bahn.innerText    = (Strecke * Bahn_C);
-    a_Flugzeug.innerText= (Strecke * Flugzeug_C);
-    a_Fahrad.innerText  = (Strecke * Fahrad_C);
+    var po_Auto = document.getElementById("proßent_Soll_Auto");
+    var po_Bahn = document.getElementById("proßent_Soll_Bahn");
+    var po_Flugzeug = document.getElementById("proßent_Soll_Flugzeug");
 
-    p_Auto.innerText    = (Strecke * Auto_C)    / Austoß_Woche;
-    p_Bahn.innerText    = (Strecke * Bahn_C)    / Austoß_Woche;
-    p_Flugzeug.innerText= (Strecke * Flugzeug_C)/ Austoß_Woche;
-    p_Fahrad.innerText  = (Strecke * Fahrad_C)  / Austoß_Woche;
+    a_Auto.innerText    = (Strecke[0] * Auto_C);
+    a_Bahn.innerText    = (Strecke[1] * Bahn_C);
+    a_Flugzeug.innerText= (Strecke[2] * Flugzeug_C);
+   
+    p_Auto.innerText    = round(((Strecke[0] * Auto_C)    / Austoß_Woche) * 100);
+    p_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche) * 100);
+    p_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche) * 100);
+    
+
+    po_Auto.innerText   = round(((Strecke[0] * Auto_C)    / Austoß_Woche_Sol) * 100);
+    po_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche_Sol) * 100);
+    po_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche_Sol) * 100);
+   
+
+    document.getElementById("austoß_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Soll_Fahrad").innerText  = "/";
+
 }
 
 function onBottumClickC() {
@@ -81,25 +106,37 @@ function onBottumClickC() {
     var a_Auto = document.getElementById("austoß_Auto");
     var a_Flugzeug = document.getElementById("austoß_Flugzeug");
     var a_Bahn = document.getElementById("austoß_Bahn");
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+   
 
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+   
     var p_Auto = document.getElementById("proßent_Auto");
-    var p_Fahrad = document.getElementById("proßent_Fahrad");
     var p_Bahn = document.getElementById("proßent_Bahn");
     var p_Flugzeug = document.getElementById("proßent_Flugzeug");
     
-    a_Auto.innerText    = (Strecke * Auto_C);
-    a_Bahn.innerText    = (Strecke * Bahn_C);
-    a_Flugzeug.innerText= (Strecke * Flugzeug_C);
-    a_Fahrad.innerText  = (Strecke * Fahrad_C);
+    var po_Auto = document.getElementById("proßent_Soll_Auto");
+    var po_Bahn = document.getElementById("proßent_Soll_Bahn");
+    var po_Flugzeug = document.getElementById("proßent_Soll_Flugzeug");
 
-    p_Auto.innerText    = (Strecke * Auto_C)    / Austoß_Woche;
-    p_Bahn.innerText    = (Strecke * Bahn_C)    / Austoß_Woche;
-    p_Flugzeug.innerText= (Strecke * Flugzeug_C)/ Austoß_Woche;
-    p_Fahrad.innerText  = (Strecke * Fahrad_C)  / Austoß_Woche;
-
+    a_Auto.innerText    = (Strecke[0] * Auto_C);
+    a_Bahn.innerText    = (Strecke[1] * Bahn_C);
+    a_Flugzeug.innerText= (Strecke[2] * Flugzeug_C);
     
+
+    p_Auto.innerText    = round(((Strecke[0] * Auto_C)    / Austoß_Woche) * 100);
+    p_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche) * 100);
+    p_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche) * 100);
+    
+
+    po_Auto.innerText   = round(((Strecke[0] * Auto_C)    / Austoß_Woche_Sol) * 100);
+    po_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche_Sol) * 100);
+    po_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche_Sol) * 100);
+    
+
+
+    document.getElementById("austoß_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Soll_Fahrad").innerText  = "/";
+
 
 }
 
@@ -112,21 +149,41 @@ function onBottumClickD() {
     var a_Auto = document.getElementById("austoß_Auto");
     var a_Flugzeug = document.getElementById("austoß_Flugzeug");
     var a_Bahn = document.getElementById("austoß_Bahn");
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+   
 
-    var a_Fahrad = document.getElementById("austoß_Fahrad");
+    
     var p_Auto = document.getElementById("proßent_Auto");
     var p_Fahrad = document.getElementById("proßent_Fahrad");
     var p_Bahn = document.getElementById("proßent_Bahn");
     var p_Flugzeug = document.getElementById("proßent_Flugzeug");
     
-    a_Auto.innerText    = (Strecke * Auto_C);
-    a_Bahn.innerText    = (Strecke * Bahn_C);
-    a_Flugzeug.innerText= (Strecke * Flugzeug_C);
-    a_Fahrad.innerText  = (Strecke * Fahrad_C);
+   
+    var po_Auto = document.getElementById("proßent_Soll_Auto");
+    var po_Fahrad = document.getElementById("proßent_Soll_Fahrad");
+    var po_Bahn = document.getElementById("proßent_Soll_Bahn");
+    var po_Flugzeug = document.getElementById("proßent_Soll_Flugzeug");
+    
+    document.getElementById("austoß_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Fahrad").innerText  = "/";
+    document.getElementById("proßent_Soll_Fahrad").innerText  = "/";
 
-    p_Auto.innerText    = (Strecke * Auto_C)    / Austoß_Woche;
-    p_Bahn.innerText    = (Strecke * Bahn_C)    / Austoß_Woche;
-    p_Flugzeug.innerText= (Strecke * Flugzeug_C)/ Austoß_Woche;
-    p_Fahrad.innerText  = (Strecke * Fahrad_C)  / Austoß_Woche;
+    a_Auto.innerText    = (Strecke[0] * Auto_C);
+    a_Bahn.innerText    = (Strecke[1] * Bahn_C);
+    a_Flugzeug.innerText= (Strecke[2] * Flugzeug_C);
+    
+
+    p_Auto.innerText    = round(((Strecke[0] * Auto_C)    / Austoß_Woche) * 100);
+    p_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche) * 100);
+    p_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche) * 100);
+
+
+    po_Auto.innerText   = round(((Strecke[0] * Auto_C)    / Austoß_Woche_Sol) * 100);
+    po_Bahn.innerText    = round(((Strecke[1] * Bahn_C)    / Austoß_Woche_Sol) * 100);
+    po_Flugzeug.innerText= round(((Strecke[2] * Flugzeug_C)/ Austoß_Woche_Sol) * 100);
+    
+}
+
+
+function round(wert){
+    return (Math.round(wert * 10)) / 10;
 }
